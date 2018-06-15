4878bdce21a3d67681ca1db1afa366a752d12ccf
Michael <micbarash@gmail.com>
2018-06-15 22:26:57 +0300

    Added bubble_sort_by method and some refactoring

--- bubble.rb
+++ bubble.rb
@@ -1,18 +1,30 @@
-class Array
-  def swap!(a, b)
-    self[a], self[b] = self[b], self[a]
-    self
-  end
-end
-
 def bubble_sort(array)
 	(array.length - 1).times do
 		for i in (0...(array.length) - 1)
-			if (array[i] <=> array[i + 1]) == 1
-				array.swap!(i,i+1)
+			a = array[i]
+			b = array[i+1]
+			if (a <=> b) == 1
+				array[i], array[i+1] = array[i+1], array[i]
 			end
 		end
 	end
 	p array
 end
 bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+	(array.length - 1).times do
+		for i in (0...(array.length) - 1)
+			a = array[i]
+			b = array[i+1]
+			result = yield(a, b)
+			if result > 0
+				array[i], array[i+1] = array[i+1], array[i]
+			end
+		end
+	end
+	p array
+end
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+	left.length - right.length
+end
