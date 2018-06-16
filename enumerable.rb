b4cd450bfa6e8dfcfc57e539305edc644647b231
Michael <micbarash@gmail.com>
2018-06-16 13:22:05 +0300

    Added my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -9,8 +9,19 @@ module Enumerable
 	def my_each_with_index
 		self.length.times do |index|
 			yield(index, self[index])
+			self
 		end
+	end
+
+	def my_select
+		copyArray = []
+		self.my_each do |elt|
+			if yield elt
+				copyArray << elt
+			end
+		end
+		copyArray
 	end
 end
 
-array = ["a","b","c"]
+array = ["a","b","c","d","e","k","i"]
